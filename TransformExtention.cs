b24ac09f7fad80b474b6a3c0744f58bde9628561
nirharpaz <38563381+nirharpaz@users.noreply.github.com>
2021-07-07 18:15:23 +0300

    Added functions related to RectTransform
    SetLeft
    SetRight
    SetTop
    SetButtom
    GetLeft
    GetRight
    GetTop
    GetButtom

--- TransformExtention.cs
+++ TransformExtention.cs
@@ -28,7 +28,7 @@ using System.Linq;
     //Depth-first search
     public static Transform FindChildDFS(this Transform Parent, string Name)
     {
-      return Parent.Find(Name) ??
+      return Parent.Find(Name);
         Parent.Children()
           .Select(child => child.FindChildDFS(Name))
           .Where(result => result != null)
@@ -246,3 +246,47 @@ using System.Linq;
       }
     }
  }
+
+public static class RectTransformExtensions
+{
+    public static void SetLeft(this RectTransform rt, float left)
+    {
+        rt.offsetMin = new Vector2(left, rt.offsetMin.y);
+    }
+
+    public static void SetRight(this RectTransform rt, float right)
+    {
+        rt.offsetMax = new Vector2(-right, rt.offsetMax.y);
+    }
+
+    public static void SetTop(this RectTransform rt, float top)
+    {
+        rt.offsetMax = new Vector2(rt.offsetMax.x, -top);
+    }
+
+    public static void SetBottom(this RectTransform rt, float bottom)
+    {
+        rt.offsetMin = new Vector2(rt.offsetMin.x, bottom);
+    }
+
+
+    public static float GetLeft(this RectTransform rt)
+    { 
+        return rt.offsetMin.x;
+    }
+
+    public static float GetRight(this RectTransform rt)
+    {
+        return -rt.offsetMax.x;
+    }
+
+    public static float GetTop(this RectTransform rt)
+    {
+        return -rt.offsetMax.y;
+    }
+
+    public static float GetBottom(this RectTransform rt)
+    {
+        return rt.offsetMin.y;
+    }
+}
